0e632c3739039ad5e4d18af887254b1ac5f37a59
fegzycole <fergusoniyara@gmail.com>
2019-12-18 08:10:11 +0100

    Add challenge 1

--- Challenges/1.sql
+++
@@ -1,8 +0,0 @@
-SELECT population FROM world
-  WHERE name = 'France';
-
-SELECT name, population FROM world
-  WHERE name IN ('Brazil', 'Russia', 'India', 'China');
-
-SELECT name, area FROM world
-  WHERE area BETWEEN 250000 AND 300000;
